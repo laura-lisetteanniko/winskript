b582043ab220e565ecf446bb480c6cee7972fe86
Laura <laura-lisette.anniko@voco.ee>
2026-02-17 13:13:31 +0200

    Valmis tehtud Practice 6 ja 7

---
+++ forloop.ps1
@@ -0,0 +1,16 @@
+﻿$colors = "Red", "Green", "Yellow", "Blue"
+$groupList = @()
+
+for ($i = 1; $i -le 20; $i++) {
+    
+    $randomColor = $colors | Get-Random
+    
+    $studentGroup = [PSCustomObject]@{
+        RollNumber = $i
+        Group      = $randomColor
+    }
+    
+    $groupList += $studentGroup
+}
+
+$groupList | Format-Table -AutoSize
